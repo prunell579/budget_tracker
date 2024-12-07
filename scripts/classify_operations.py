45effae96b970afa20d53443fd7e743f6839bb8d
prunell579 <mariocastillo6@hotmail.com>
2024-12-07 14:58:47 +0100

    add of script for classifying operations and update database.json

---
+++ scripts/classify_operations.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.path.append('.')
+import model.boursorama_parser as bourso_parser
+import model.operations as ops
+
+if __name__ == '__main__':
+    user_db = ops.OperationsDatabase.load_from_json()
+
+    category_options_output_string = ''
+    i = 1
+    opt_to_category = {}
+    for category in ops.Operation.SupportedCategories:
+        category_options_output_string += str(i) + ' - ' + category.value + '\n'
+        opt_to_category[i] = category.value
+        i += 1
+
+    for op in user_db.get_operations():
+        if op.category is None:
+            op.summary()
+            category_choice = input(category_options_output_string)
+            op.category = ops.Operation.SupportedCategories(opt_to_category[int(category_choice)])
+        break
+
+    # update db with category info
+    user_db.write_to_file()
+
+    
+
